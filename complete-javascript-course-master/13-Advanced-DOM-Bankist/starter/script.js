2dc0bc3f7acfc341f69937a10ab049d5a7ca8fc8
hieunugent <hieunugent@gmail.com>
2023-08-29 23:19:32 -0700

    update

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -217,32 +217,7 @@ const handleHover = function (e, opacity) {
 };
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
-// Sticky navigation
 
-// const initialCoords = section1.getBoundingClientRect();
-// console.log(initialCoords);
-// window.addEventListener('scroll', function(e){
-//   console.log(window.scrollY);
-//  if(this.window.scrollY > initialCoords.top){
-//   nav.classList.add('sticky');
-
-// }else{
-//   nav.classList.remove('sticky');
-// }
-// });
-// Sticky Navigation: intersection Observer API:
-// const obsCallback = function(entries, observer){
-//     entries.forEach(entry=> {
-//       console.log(entry);
-//     })
-// }
-// const  obsOptions = {
-//   root:null,
-//   rootMargin:"0.0px",
-//   threshold:[0,0.2]
-// };
-// const observer = new IntersectionObserver(obsCallback,obsOptions);
-// observer.observe(section1);
 
 const header = document.querySelector('.header');
 const stickyNav = function (entries) {
@@ -297,75 +272,111 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img));
 
 // slider
-const slider = function(){
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
 
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-const dotContainer = document.querySelector('.dots');
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
 
-let curSlide = 0;
-const maxSlide = slides.length;
-
-slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
-
-const gotoSlice = function (slide) {
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-  );
-};
-const nextSlide = function () {
-  if (curSlide === maxSlide - 1) {
-    curSlide = 0;
-  } else curSlide++;
-  gotoSlice(curSlide);
-  activateDot(curSlide);
-};
-const prevSlide = function () {
-  if (curSlide === 0) curSlide = maxSlide - 1;
-  else curSlide--;
-  gotoSlice(curSlide);
-  activateDot(curSlide);
-};
-const createDots = function () {
-  slides.forEach(function (_, i) {
-    dotContainer.insertAdjacentHTML(
-      'beforeend',
-      `<button class="dots__dot" data-slide="${i}"></button>`
+  const gotoSlice = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
     );
+  };
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else curSlide++;
+    gotoSlice(curSlide);
+    activateDot(curSlide);
+  };
+  const prevSlide = function () {
+    if (curSlide === 0) curSlide = maxSlide - 1;
+    else curSlide--;
+    gotoSlice(curSlide);
+    activateDot(curSlide);
+  };
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+  const init = function () {
+    gotoSlice(0);
+
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+  // next slice
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+  document.addEventListener('keydown', function (e) {
+    console.log(e);
+    e.key === 'ArrowLeft' && prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset.slide;
+      gotoSlice(slide);
+      activateDot(slide);
+    }
   });
 };
-const activateDot = function (slide) {
-  document
-    .querySelectorAll('.dots__dot')
-    .forEach(dot => dot.classList.remove('dots__dot--active'));
-  document
-    .querySelector(`.dots__dot[data-slide="${slide}"]`)
-    .classList.add('dots__dot--active');
-};
-const init = function () {
-  gotoSlice(0);
-
-  createDots();
-
-  activateDot(0);
-};
-init();
-// next slice
-btnRight.addEventListener('click', nextSlide);
-btnLeft.addEventListener('click', prevSlide);
-document.addEventListener('keydown', function (e) {
-  console.log(e);
-  e.key === 'ArrowLeft' && prevSlide();
-  e.key === 'ArrowRight' && nextSlide();
-});
-dotContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('dots__dot')) {
-    const { slide } = e.target.dataset.slide;
-    gotoSlice(slide);
-    activateDot(slide);
-  }
-});
-}
 slider();
+// Sticky navigation
+
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+// window.addEventListener('scroll', function(e){
+//   console.log(window.scrollY);
+//  if(this.window.scrollY > initialCoords.top){
+//   nav.classList.add('sticky');
+
+// }else{
+//   nav.classList.remove('sticky');
+// }
+// });
+// Sticky Navigation: intersection Observer API:
+// const obsCallback = function(entries, observer){
+//     entries.forEach(entry=> {
+//       console.log(entry);
+//     })
+// }
+// const  obsOptions = {
+//   root:null,
+//   rootMargin:"0.0px",
+//   threshold:[0,0.2]
+// };
+// const observer = new IntersectionObserver(obsCallback,obsOptions);
+// observer.observe(section1);
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('HTML parsed and DOM tree build!', e);
+});
+window.addEventListener('load', function(e){
+  console.log('window is fully loaded', e);
+});
+// only use when the page is need to save before living
+// window.addEventListener('beforeunload', function(e){
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue='';
+// });

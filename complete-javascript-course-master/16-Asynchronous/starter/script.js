c2a1769c3b6b88a6964ed284c241478ef995804d
hieunugent <hieunugent@gmail.com>
2023-09-22 18:08:49 -0700

    update

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/* most code is synchronous 
+    synchronous code is executed line by line
+    each line of code waits for previous line to finish
+*/

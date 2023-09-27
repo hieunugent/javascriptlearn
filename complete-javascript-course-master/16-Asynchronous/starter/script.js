8a530a54d1c998522914db2a66444132afd84e90
hieunugent <hieunugent@gmail.com>
2023-09-26 23:41:19 -0700

    update

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,7 +4,25 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-/* most code is synchronous 
-    synchronous code is executed line by line
-    each line of code waits for previous line to finish
+/* 
+
+most code is synchronous 
+synchronous code is executed line by line
+each line of code waits for previous line to finish
+Asynchronous code is executed  after a task that run in the background finishes;
+Asynchronous code is non-blocking 
+Execution doesn't wait for an asynchronous task to finish its wordBreak: 
+Callback functions alone do not make code asynchronous 
+
+AJAX
+Asynchronous JS And XML: allow us to communicate with remote web servers 
+in an asynchronous way. With AJAX calls, we can request data from web 
+servers dynamically 
+Application Programming Interface: Piece of software that can be used by 
+another piece of software, in order to allow application to talk to each 
+other;
+There are be many types of APIs on web development: DOM API, Geolocation 
+API
+https://countries-api-836d.onrender.com/countries/
+
 */

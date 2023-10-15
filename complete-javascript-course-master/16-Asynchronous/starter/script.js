87eac49cd1ed91b19f48081734fe4d76afe9f5ac
hieunugent <hieunugent@gmail.com>
2023-10-14 22:28:32 -0700

    udpate

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -197,38 +197,38 @@ const getCountryData = function (country) {
 // }
 // }
 
-const imgContainer = document.querySelector('.images')
- const createImage = function(imgPath){
-  return new Promise(function(resolve, reject){
-    const img = document.createElement('img');
-    img.src = imgPath;
-    img.addEventListener('load', function(){
-      imgContainer.append(img);
-      resolve(img);
-    });
-    img.addEventListener('error', function(){
-      reject(new Error('Image not Found'))
-    });
+// const imgContainer = document.querySelector('.images')
+//  const createImage = function(imgPath){
+//   return new Promise(function(resolve, reject){
+//     const img = document.createElement('img');
+//     img.src = imgPath;
+//     img.addEventListener('load', function(){
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
+//     img.addEventListener('error', function(){
+//       reject(new Error('Image not Found'))
+//     });
 
-  });
- }
- const wait = function (ms) {
-   return new Promise(resolve => setTimeout(resolve, ms*1000))
- };
-  let currentImg 
- createImage('img/img-1.jpg').then(img => {
-   currentImg = img;
-   console.log('Image 1 loaded');
-   return wait(2);
- }).then(()=>{
-  currentImg.style.display = 'none';
-  return createImage('img/img-2.jpg')
+//   });
+//  }
+//  const wait = function (ms) {
+//    return new Promise(resolve => setTimeout(resolve, ms*1000))
+//  };
+//   let currentImg 
+//  createImage('img/img-1.jpg').then(img => {
+//    currentImg = img;
+//    console.log('Image 1 loaded');
+//    return wait(2);
+//  }).then(()=>{
+//   currentImg.style.display = 'none';
+//   return createImage('img/img-2.jpg')
 
- }).then(img => {
-    currentImg = img;
-    console.log('Image 2 loaded');
-    return wait(2);
- }).then(()=> {
-  currentImg.style.display = 'none';
- }).catch(err => console.error(err));
+//  }).then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//  }).then(()=> {
+//   currentImg.style.display = 'none';
+//  }).catch(err => console.error(err));
 

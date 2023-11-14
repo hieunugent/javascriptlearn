0d7298fc62fea5d06eefa1f3c7679d4cbcb6b63d
hieunugent <hieunugent@gmail.com>
2023-11-13 23:18:13 -0800

    update:

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,4 +1,6 @@
 import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime'
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -25,10 +27,13 @@ const renderSpinner = function (parentEl) {
 };
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if(!id) return ;
     // loading spinner
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -152,4 +157,5 @@ const showRecipe = async function () {
     alert(error);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe))
---
+++ complete-javascript-course-master/18-forkify/starter/src/js/model.js
@@ -0,0 +1,4 @@
+export const state = {
+    recipe :{}
+};
+export const loadRecipe = async function

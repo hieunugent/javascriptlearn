98c927ec11d9f563d57f84622f0be8b790c4391b
hieunugent <hieunugent@gmail.com>
2023-11-16 22:06:57 -0800

    update:

--- complete-javascript-course-master/18-forkify/starter/src/js/helpers.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/helpers.js
@@ -3,6 +3,7 @@ export const getJSON = async function (url){
         
     const res = await fetch(url);
     const data = await res.json();
+    return data;
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     } catch (error) {
         

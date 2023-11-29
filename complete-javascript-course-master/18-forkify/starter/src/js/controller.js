a0c2cc2dc8c3a7e30e2c1b254aede817a2dfff63
hieunugent <hieunugent@gmail.com>
2023-11-28 21:11:56 -0800

    update

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -3,7 +3,7 @@ import { async } from 'regenerator-runtime';
 import * as model from './model.js'
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
-
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
@@ -15,9 +15,9 @@ const recipeContainer = document.querySelector('.recipe');
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-if (module.hot){
-  module.hot.accept()
-}
+// if (module.hot){
+//   module.hot.accept()
+// }
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -39,15 +39,22 @@ const controlRecipes = async function () {
 const controlSearchResults = async function(){
   try {
     resultView.renderSpinner();
-    
+
     //  1 get search query
     const query = searchView.getQuery();
-    if(!query) return ;
+    if (!query) return;
     // 2 load search results;
     await model.loadSearchResults(query);
     // 3. render results
-    console.log(model.state.search.results);
-    resultView.render(model.state.search.results);
+    // console.log(model.state.search.results);
+    console.log(model.getSearchResultsPage());
+
+    // resultView.render(model.state.search.results);
+    resultView.render(model.getSearchResultsPage());
+
+    // 4. render initial pagination button  
+    paginationView.render(model.state.search)
+
   } catch (error) {
     console.log(error);
   }
--- complete-javascript-course-master/18-forkify/starter/src/js/model.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/model.js
@@ -1,13 +1,15 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE} from './config';
 import { getJSON } from './helpers';
 export const state = {
   recipe: {},
   search: {
     query: '',
+    page:1,
     results: [],
+    resultsPerPage: RES_PER_PAGE,
   },
-};
+}
 export const loadRecipe = async function (id) {
   try {
     const data = await getJSON(`${API_URL}/${id}`);
@@ -49,3 +51,10 @@ export const loadSearchResults = async function (query) {
   }
 };
 
+export const getSearchResultsPage = function(page = state.search.page){
+  state.search.page = page;
+  const start = (page -1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  console.log(start, end);
+  return state.search.results.slice(start, end);
+}
